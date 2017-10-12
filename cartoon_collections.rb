640d0cedd091a447482452ce277fae7caa617f7b
Alicia Yu <alicia.yu.16@gmail.com>
2017-10-12 21:31:00 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ end
 
 def summon_captain_planet(array =planeteer_calls)# code an argument here
   # Your code here
-  words = planeteer_calls.split(" ")
+  words = array.split(" ")
 
   index = 0
   while index < words.length
