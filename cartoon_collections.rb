5e5b63a9887eb1c29e56ee21780ca0f11f6b8f46
Alicia Yu <alicia.yu.16@gmail.com>
2017-10-12 22:09:03 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ end
 def summon_captain_planet(array =planeteer_calls)# code an argument here
   # Your code here
   array.map!(&:capitalize)
-  array
+  array"!"
 end
 
 def long_planeteer_calls# code an argument here
